ea9d07b0efff86409db30badadaabb54cea284b4
Sean Jones <web@sean-jones.co.uk>
2021-03-03 12:24:09 +0000

    Feature/tailwind css (#2)
    * add tailwind for styling
    
    * correct dir name typo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from "react";
 
+import "../../styles/app.scss";
+
 const App: FC = () => (
-  <div className="app">
+  <div className="app bg-gray-50 min-h-screen">
     <p>App</p>
   </div>
 );
