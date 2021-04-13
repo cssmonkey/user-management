f507e5010dd8ff7b7a49ad34b90e4eeb2dc812f3
Sean Jones <web@sean-jones.co.uk>
2021-04-13 11:10:30 +0100

    add placholder logo and site header component (#7)

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
 
 const Loader: FC = () => (
-  <div className="loader">
-    <p>Loading...</p>
+  <div className="loader flex justify-center items-center min-h-screen">
+    <div className="loader__inner">
+      <p className="text-3xl text-gray-600 animate-pulse">Loading...</p>
+    </div>
   </div>
 );
 
